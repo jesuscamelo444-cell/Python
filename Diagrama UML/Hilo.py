72737d1adbd33c912fabfc755de28ceafad8441f
jesuscamelo444-cell <jesuscamelo444@gmail.com>
2025-10-16 16:20:27 -0500

    Delete Diagrama UML/Hilo.py

--- Diagrama UML/Hilo.py
+++
@@ -1,6 +0,0 @@
-        #class hilo
-class Hilo:
-    def busca_novedades(self, recolector, editorial):
-        print("Hilo: buscando novedades...")
-        recolector.envia_novedades(editorial)
-        print("Hilo: proceso de envío de novedades completado.")
