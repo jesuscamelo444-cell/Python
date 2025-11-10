daa3fd3aae864e30b1e155535227b8d6406cf892
jesuscamelo444-cell <jesuscamelo444@gmail.com>
2025-11-09 21:47:51 -0500

    Delete Animales/Animales.py

--- Animales/Animales.py
+++
@@ -1,9 +0,0 @@
-class Animal:
-    def __init__(self, Nombre, Edad, Habita, Dieta, Tamaño, Color):
-        self.Nombre = Nombre
-        self.Edad = Edad
-        self.Habita = Habita
-        self.Dieta = Dieta
-        self.Tamaño = Tamaño
-        self.Color = Color  
-        
